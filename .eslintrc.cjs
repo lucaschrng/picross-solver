67db189f83bbc32f6823e46baa271b6a02706db1
Lucas Charoing <charoing.lucas@gmail.com>
2024-01-11 11:01:41 +0100

    Setup project

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {browser: true, es2020: true},
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
@@ -8,11 +8,12 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: [
+    '@stylistic'
+  ],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'quotes': ['error', 'single'],
+    '@stylistic/indent': ['error', 2],
+    'object-curly-spacing': ['error', 'always'],
   },
 }
